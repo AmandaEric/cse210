04da4e7ee5761dd00fb088cb7e509b2a2dd6c84a
Amanda Ericson <eri24017@byui.edu>
2025-04-30 22:19:37 -0600

    Part 1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,4 +6,70 @@ class Program
     {
         Console.WriteLine("Hello Prep1 World!");
     }
+}\
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+    }
+}
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        double percentage = double.Parse(input);
+
+        string letter;
+
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! Keep working hard and you'll do better next time.");
+        }
+    }
 }
